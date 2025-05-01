85a0437f9148d633ef20bd86c9ea4f077fd2ed59
mohmmadView <mamad.rahgoshahi@gmail.com>
2025-05-01 06:47:52 +0330

    app.get(res.sendFile())

--- myApp.js
+++ myApp.js
@@ -2,10 +2,11 @@ let express = require('express');
 //console.log("Hello World");
 
 let app = express();
+let absolutePath = __dirname + '/relativePath/file.ext'
 app.get("/",function(req,res)
         { 
-         res.send("Hello Express")
-      res.sendFile(__dirname+"/views/file.txt")
+         //res.send("Hello Express")
+      res.sendFile(absolutePath)
         }
        );
 
