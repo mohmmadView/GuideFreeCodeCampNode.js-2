38d9085e00dfc73a86b6013948a8f00fb05d0418
mohmmadView <mamad.rahgoshahi@gmail.com>
2025-05-01 10:27:58 +0330

    app.use(express.static(path.join())

--- myApp.js
+++ myApp.js
@@ -1,14 +1,26 @@
 let express = require('express');
+const { log } = require('fcc-express-bground');
 //console.log("Hello World");
-
+let path = require('path');
+log(path)
 let app = express();
-let absolutePath = __dirname + '/relativePath/file.ext'
+// let absolutePath = __dirname + '/relativePath/file.ext'
+       app.use('/public',express.static(path.join(__dirname , 'public')));
 app.get("/",function(req,res)
         { 
-         //res.send("Hello Express")
-      res.sendFile(absolutePath)
+        //  res.send("Hello Express")
+      res.sendFile(path.join(__dirname ,'views','index.html'))
         }
        );
+
+
+      //  app.get('/',(req,res)=>{
+      //   res.sendFile(path.join(__dirname,'view','index.html'))
+      //  })
+      //  app.listen(3000,()=>{
+      //   console.log('server is runnig on port 3000');
+        
+      //  });
 
 
 
