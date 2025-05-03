43758220fe180f0b6487383785fdc5cbdf2a2b27
mohmmadView <mamad.rahgoshahi@gmail.com>
2025-05-03 04:08:35 +0330

    fix bug

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ app.get("/",function(req,res)
         }
        );
        app.get("/json",function(req,res){
-        res.json({"message":"Hello Node.js"})
+        let dataJson = res.json({message:"Hello json"});
         
        })
 
