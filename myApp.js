ab6765c78bda420c931449686ba325e432a7f333
mohmmadView <mamad.rahgoshahi@gmail.com>
2025-05-05 05:11:14 +0330

    fix to upper case

--- myApp.js
+++ myApp.js
@@ -2,16 +2,13 @@ require('dotenv').config();
 let express = require('express');
 const {json} =require('body-parser')
 const { log } = require('fcc-express-bground');
-//console.log("Hello World");
 let path = require('path');
 log("path",path.dirname)
 let app = express();
-// let absolutePath = __dirname + '/relativePath/file.ext'
        app.use('/public',express.static(path.join(__dirname , 'public')));
        
 app.get("/",function(req,res)
         { 
-        //  res.send("Hello Express")
       res.sendFile(path.join(__dirname ,'views','index.html'))
         }
        );
